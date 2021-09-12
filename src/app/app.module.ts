517126ae5c75b1c194ed78e660527d2de8da96b4
Filipe Silva <filipeqs@outlook.com>
2021-09-12 14:43:49 -0400

    fetching and saving recipes to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -32,7 +33,14 @@ import { RecipeService } from './recipes/recipe.service';
         RecipeStartComponent,
         RecipeEditComponent,
     ],
-    imports: [BrowserModule, FormsModule, ReactiveFormsModule, RouterModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule,
+        HttpClientModule,
+        AppRoutingModule,
+    ],
     providers: [ShoppingListService, RecipeService],
     bootstrap: [AppComponent],
 })
