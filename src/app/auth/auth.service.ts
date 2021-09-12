5a1caebea967bc09feea262218b472e92c0a9e14
Filipe Silva <filipeqs@outlook.com>
2021-09-12 19:40:54 -0400

    auto login and logout, auth guard

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,6 +19,7 @@ export interface AuthResponseData {
 })
 export class AuthService {
     user = new BehaviorSubject<User>(null);
+    private tokenExpirationTimer: any;
     apiKey = 'AIzaSyAgPjWpVR0krNgyNOzp_iSDK7xEhCrUCXQ';
 
     constructor(private http: HttpClient, private router: Router) {}
@@ -69,14 +70,52 @@ export class AuthService {
             );
     }
 
+    autoLogin() {
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) return;
+
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate),
+        );
+
+        if (loadedUser.token) {
+            const expirationDuration =
+                new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+            this.autoLogout(expirationDuration);
+            this.user.next(loadedUser);
+        }
+    }
+
     logout() {
         this.user.next(null);
+        localStorage.removeItem('userData');
+        if (this.tokenExpirationTimer) {
+            clearTimeout(this.tokenExpirationTimer);
+        }
+        this.tokenExpirationTimer = null;
         this.router.navigate(['/auth']);
+    }
+
+    autoLogout(expirationDuration: number) {
+        this.tokenExpirationTimer = setTimeout(() => {
+            this.logout();
+        }, expirationDuration);
     }
 
     private handleAuthentication(email: string, userId: string, expiresIn: number, token: string) {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
+
+        localStorage.setItem('userData', JSON.stringify(user));
+        this.autoLogout(expiresIn * 1000);
 
         this.user.next(user);
     }
